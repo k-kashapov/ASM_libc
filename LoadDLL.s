d3e93418a9806d9b71c2a03a7270e7ff6f7026b1
k-kashapov <vanyasik247@gmail.com>
2022-03-20 15:29:23 +0300

    8 args printf using stack

--- LoadDLL.s
+++ LoadDLL.s
@@ -4,20 +4,29 @@ extern printf, fflush, stdout
 section .text
 
 _start:
+	push 0x32
+	push 0x42
+	push 0x9b
+	mov r9,  5
+	mov r8,  4
+	mov rcx, 3
+	mov rdx, 2
 	mov rsi, 13
-	
 	lea rdi, [String]
+	
 	mov rax, 0
 
 	call printf
 
+	times 3 pop rax
+
 	mov rdi, [stdout]
 	call fflush
-	
+
 	mov rax, 0x3c	; exit (rdi)
 	xor rdi, rdi
 	syscall
 
 section .data
 
-String db 'Hello World %d'
+String db 'Hello World %d %x %x %x %x %x %x %x'
